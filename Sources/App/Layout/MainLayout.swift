fbda8a8c0f4381e24fc32b406db156da51e97ad9
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2024-10-27 14:52:11 -0400

    Implement home page

--- Sources/App/Layout/MainLayout.swift
+++ Sources/App/Layout/MainLayout.swift
@@ -1,5 +1,41 @@
 import Elementary
 
+private let tailwindConfig = """
+	tailwind.config = {
+		theme: {
+			extend: {
+				colors: {
+					'black': '#282c34',
+					'white': '#abb2bf',
+					'light-red': '#e06c75',
+					'dark-red': '#be5046',
+					'green': '#98c379',
+					'light-yellow': '#e5c07b',
+					'dark-yellow': '#d19a66',
+					'blue': '#61afef',
+					'magenta': '#c678dd',
+					'cyan': '#56b6c2',
+					'gutter-grey': '#4b5263',
+					'comment-grey': '#5c6370'
+				}
+			}
+		}
+	}
+	"""
+
+private let tailwindCSS = """
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+	@layer base {
+		html, body {
+			@apply bg-black;
+			@apply text-white;
+		}
+	}
+	"""
+
 extension MainLayout: Sendable where Content: Sendable {}
 struct MainLayout<Content: HTML>: HTMLDocument {
 	var title: String
@@ -12,13 +48,12 @@ struct MainLayout<Content: HTML>: HTMLDocument {
 		meta(.name(.viewport), .content("width=device-width, initial-scale=1.0"))
 		meta(.title(title))
 
-		// TODO: Use Tailwind CLI to generate CSS instead
 		script(.src("https://cdn.tailwindcss.com")) {}
+		script { tailwindConfig }
+		style(.custom(name: "type", value: "text/tailwindcss")) { tailwindCSS }
 	}
 
 	var body: some HTML {
-		main {
-			pageContent
-		}
+		pageContent
 	}
 }
--- Sources/App/Pages/HomePage.swift
+++ Sources/App/Pages/HomePage.swift
@@ -5,10 +5,32 @@ struct HomePage: HTML {
 	let fluent: Fluent
 
 	var content: some HTML {
+		main(
+			.class(
+				"mx-auto flex min-h-screen w-full max-w-[400px] flex-col justify-center overflow-hidden bg-black"
+			)
+		) {
+			div(.class("w-full")) {
+				h1(.class("mb-2 text-3xl font-semibold")) {
+					"Channels"
+				}
+			}
+
+			div(
+				.class(
+					"relative flex h-full flex-col overflow-scroll rounded-lg bg-comment-grey bg-clip-border shadow-md"
+				)
+			) {
+				TableContent()
+			}
+		}
+	}
+
+	private func TableContent() -> some HTML {
 		AsyncContent {
 			let channels = try await Stat.query(on: fluent.db()).all()
-
-			let channelCounts: [String: Int] = channels.reduce(into: [:]) { partialResult, stat in
+			let channelCounts: [String: Int] = channels.reduce(into: [:]) {
+				partialResult, stat in
 				guard let channel = stat.channel else { return }
 
 				if !partialResult.keys.contains(channel) {
@@ -18,18 +40,73 @@ struct HomePage: HTML {
 				partialResult[channel]! += 1
 			}
 
-			h1 { "Hello there" }
+			if !channelCounts.isEmpty {
+				Table(
+					headers: ["Channel", "Messages", ""],
+					rows: channelCounts.map { [$0.key, "\($0.value)", $0.key] }
+				) { i, text in
+					switch i {
+					case 0, 1:
+						p(.class("text-sm")) { text }
+					case 2:
+						Button(href: "/channel/\(text)", text: "See stats")
+					default:
+						fatalError("Shouldn't get here!")
+					}
+				}
+			} else {
+				// TODO: pull the channels from the server config
+				p(.class("p-4")) { "No messages recorded" }
+			}
+		}
+	}
+}
 
-			ul {
-				for (channel, count) in channelCounts {
-					li {
-						a(.href("/channel/\(channel)")) {
-							channel
+private struct Table<CellContent: HTML>: HTML {
+	let headers: [String]
+	let rows: [[String]]
+
+	@HTMLBuilder
+	var cellContent: (Int, String) -> CellContent
+
+	var content: some HTML {
+		table(.class("table-auto text-left")) {
+			thead {
+				tr {
+					for header in headers {
+						th(.class("border-b border-slate-600 bg-gutter-grey p-4")) {
+							p(.class("text-sm font-semibold")) { header }
 						}
-						": \(count)"
+					}
+				}
+			}
+
+			tbody {
+				for row in rows {
+					tr {
+						for (i, text) in row.enumerated() {
+							td(.class("p-4")) {
+								cellContent(i, text)
+							}
+						}
 					}
 				}
 			}
 		}
+	}
+}
+
+private struct Button: HTML {
+	let href: String
+	let text: String
+
+	var content: some HTML {
+		a(
+			.href(href),
+			.class(
+				"rounded-md border border-slate-300 px-4 py-2 text-center text-sm shadow-sm transition-all hover:border-slate-800 hover:bg-black hover:text-white hover:shadow-lg focus:border-slate-800 focus:bg-black focus:text-white active:border-slate-800 active:bg-black active:text-white disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+			),
+			.custom(name: "type", value: "button")
+		) { text }
 	}
 }
